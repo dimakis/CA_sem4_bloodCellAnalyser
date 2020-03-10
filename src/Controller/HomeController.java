6fb335b9ecf2221bd10821cab3947a94d8a36a7a
Dimitri Saridakis <dimitri.saridakis@gmail.com>
2020-03-10 16:27:17 +0000

    work starting on counting individual cells

--- src/Controller/HomeController.java
+++ src/Controller/HomeController.java
@@ -42,10 +42,13 @@ public class HomeController implements Initializable {
     public Pane edImagePane2;
     public Pane ogImagePane2;
 
+    public int index = 0, cellCount = 0;
+    public Button countCellsBtn;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         File file = new File("/home/dimdakis/Pictures/bloodCellsAssignment1/bloodCells4.jpg");
-        Image im = new Image(file.toURI().toString());
+        Image im = new Image(file.toURI().toString(), 400, 400, false, false);
         imageView.setImage(im);
         imageViewEdited.setImage(im);
         setOpenFinder();
@@ -58,6 +61,7 @@ public class HomeController implements Initializable {
         setSquareBloodCellsBtn();
         noiseReduction();
         setDrawRectangle();
+        setCountCellsBtn();
         quit();
     }
 
@@ -69,12 +73,13 @@ public class HomeController implements Initializable {
                 File file = fileChooser.showOpenDialog(pStage);
                 fileSize = file.length();
                 System.out.println("FileSize : " + fileSize);
-                Image im = new Image(file.toURI().toString());
+                Image im = new Image(file.toURI().toString(), 400, 400, false, false);
                 imageView.setImage(im);
-                imageView.setPreserveRatio(true);
+//                imageView.setPreserveRatio(true);
                 imageViewEdited.setImage(im);
-                imageViewEdited.setPreserveRatio(true);
+//                imageViewEdited.setPreserveRatio(true);
                 imageMeta();
+//                edImagePane2.getChildren().removeAll();
             } catch (Exception ignored) {
             }
         });
@@ -92,6 +97,7 @@ public class HomeController implements Initializable {
             Image im = imageViewEdited.getImage();
             System.out.println("im url: " + im.getUrl());
             im = new Image(im.getUrl(), 144, 144, false, false);
+
             imageViewEdited.setImage(im);
         });
     }
@@ -125,15 +131,53 @@ public class HomeController implements Initializable {
                 }
             }
             //print array to console
-            for (int i = 0; i < arr.length - 1; i++) {
-
-                if (i % imageViewEdited.getImage().getWidth() == 0) {
-                    System.out.println();
-                }
-                System.out.print(Find.findIntArr(arr, i) + " ");
-            }
+//            for (int i = 0; i < arr.length - 1; i++) {
+//
+//                if (i % imageViewEdited.getImage().getWidth() == 0) {
+//                    System.out.println();
+//                }
+//                System.out.print(Find.findIntArr(arr, i) + " ");
+//            }
         }
     }
+
+    public void setCountCellsBtn() {
+        countCellsBtn.setOnAction(e -> {
+            int whiteCellCount = countCells(whiteCellMap);
+            int redCellCount = countCells(redCellMap);
+            System.out.println("whiteCell count: " + whiteCellCount + ", RedCell count: " + redCellCount);
+        });
+    }
+
+    public int countCells(HashMap<Integer, Integer> hmap) {
+//        var ref = new Object() {
+//            int count = 0;
+//        };
+        hmap.forEach((k, v) -> {
+            if (v > 50) {
+                cellCount ++;
+            }
+        });
+        return cellCount;
+    }
+
+//    public void countCells(HashMap<Integer, Integer> hmap) {
+//        countCellsBtn.setOnAction(
+//                hmap.forEach((k, v) -> {
+//                    System.out.println("key: " + k + ", v" + v);
+//                    int vTotal = 0;
+//                    if (v > 10) {
+//                        vTotal += v;
+//                        vTotal = vTotal / hmap.size();
+//                    }
+////                    cellCount = vTotal;
+//
+////                    System.out.println("cellCount: " + cellCount);
+//
+//                    })
+//        );
+////        return cellCount;
+//    }
 
     public void setSquareBloodCellsBtn() {
         squareBloodCellsBtn.setOnAction(e -> {
@@ -145,80 +189,82 @@ public class HomeController implements Initializable {
 
     public void setDrawRectangle() {
         rectangleBtn.setOnAction(e -> {
+            Image im = imageViewEdited.getImage();
+
+//            Rectangle rectangle = new Rectangle(0, 0, imageViewEdited.getFitWidth(), imageViewEdited.getFitHeight());
+//            edImagePane.getChildren().add(rectangle);
             redCellMap = arrayToHashMap(redCellArray);
-            drawRectangles(redCellArray, redCellMap,Color.GREEN);
+            drawRectangles(redCellArray, redCellMap, Color.GREEN, (int) noiseSlider.getValue());
+            whiteCellMap = arrayToHashMap(whiteCellArray);
+            drawRectangles(whiteCellArray, whiteCellMap, Color.BLUE, (int) noiseSlider.getValue());
+            System.out.println("array" + redCellArray.length);
         });
     }
 
-    public void drawRectangles(int[] arr, HashMap<Integer, Integer> rootMap, Color color) {
-        Image im = imageViewEdited.getImage();
-        int width = (int) im.getWidth();
-        int height = (int) im.getHeight();
-//        int cellWidth = 0;
-//        int cellHeight = 0;
-//        int cellWidth = 0;
-        Pane rectPane = edImagePane;
+    public void drawRectangles(int[] arr, HashMap<Integer, Integer> rootMap, Color color, int percentNoiseReduction) {
+
+        Image image = imageViewEdited.getImage();
+        double aspectRatio = image.getWidth() / image.getHeight();
+        double width = Math.min(imageViewEdited.getFitWidth(), imageViewEdited.getFitHeight() * aspectRatio);
+        double height = Math.min(imageViewEdited.getFitHeight(), imageViewEdited.getFitWidth() / aspectRatio);
+        edImagePane.resize(400, 400);
+        //        System.out.println("Height: " + height);
+        edImagePane.setLayoutX(0);
+        edImagePane.setLayoutY(0);
         rootMap.forEach((k, v) -> {
-            int cellWidth = 0;
-            int cellHeight = 0;
-            if (v > 10) {
-//                int xRoot = Find.findIntArr(arr,k);
-                int xRoot = Find.findIntArr(arr, k) % width;
-                int yRoot = (Find.findIntArr(arr,k) - xRoot)/height ;
-                for (int i = 0; i < arr.length - 1; i++) {
-                    if (arr[i] < arr.length-1 && arr[i] > -1) {
-                        if (arr[i] == arr[i + 1])
-                            cellWidth++;
-                        if ( arr[i] < arr.length-width && arr[i] == arr[i + width]) {
-                            cellHeight++;
+            boolean initialHeight = false;
+            int cellWidth = 1;
+            int cellHeight = 1;
+            int yRoot = 0;
+            int xRoot = -1;
+            if (v > percentNoiseReduction) {
+                for (int i = 0; i < arr.length - 1; i++)
+                    if (Find.findIntArr(arr, i) == k) {
+                        int x = i % (int) width, y = i / (int) width;
+                        if (xRoot == -1) {
+                            xRoot = x;
+                            yRoot = y;
+                        } else {
+                            if (x > xRoot + cellWidth) cellWidth = x - xRoot;
+                            if (x < xRoot) {
+                                cellWidth += (xRoot - x);
+                                xRoot = x;
+                            }
+                            if (y > yRoot + cellHeight) cellHeight = y - yRoot;
                         }
-//                        cellWidth++;
                     }
-                }
+//                Rectangle rectangle2 = new Rectangle(0, 0, 400, 400);
                 Rectangle rectangle = new Rectangle(xRoot, yRoot, cellWidth, cellHeight);
                 rectangle.setFill(Color.TRANSPARENT);
                 rectangle.setStroke(color);
-                edImagePane2.getChildren().add(rectangle);
+                index++;
+                Label label = new Label();
+                label.setLayoutX(xRoot + cellWidth - 18);
+                label.setLayoutY(yRoot + cellHeight - 15);
+                label.setStyle("-fx-font-weight: bold");
+                label.setText(String.valueOf(index));
+                edImagePane.getChildren().add(label);
+
+                //                edImagePane.getChildren().add(rectangle2);
                 edImagePane.getChildren().add(rectangle);
-                rectPane.getChildren().add(rectangle);
+//                edImagePane.getChildren().add(rectangle);
+//                rectPane.getChildren().add(rectangle);
 //                System.out.println("x: " + x + ", y: " + y);
-//                xRoot
-                int recWidth = 0;
+//                    }initialHeight = false;
             }
-        });
-
-
-//        for (int y = 0; y < im.getHeight(); y++) {
-//            for (int x = 0; x < im.getWidth(); x++) {
-//
-//                //union for non white pixels
-//                if (arr[(y * width) + x] >= 0) {
-//
-//                    //union beside
-//                    if ((arr[y * width + (x + 1)]) % width != 0 && arr[y * width + (x + 1)] >= 0 && arr[y * width + x] == arr[y * width + (x + 1)])
-//rect
-//
-//                    //union below
-//                    if (id + width < arr.length && arr[id + width] >= 0)
-//                        Union.quickUnion(arr, id, id + width);
-//                }
 //            }
-//        }
-//        rectPane.getChildren().add(rect());
-
-
-//        System.out.println("cellMap of array " + arr + " " + cellMap.asMap());
+        });
     }
 
     public HashMap<Integer, Integer> arrayToHashMap(int[] arr) {
         HashMap<Integer, Integer> cellMap = new HashMap<>();
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] != -1) {
-                if (!cellMap.containsKey(arr[i])) {
-                    cellMap.put(arr[i], 1);
-                } else if (cellMap.containsKey(arr[i])) {
-                    int cells = cellMap.get(arr[i]);
-                    cellMap.put(arr[i], cells + 1);
+                if (!cellMap.containsKey(Find.findIntArr(arr, i))) {
+                    cellMap.put(Find.findIntArr(arr, i), 1);
+                } else if (cellMap.containsKey(Find.findIntArr(arr, i))) {
+                    int cells = cellMap.get(Find.findIntArr(arr, i));
+                    cellMap.put(Find.findIntArr(arr, i), cells + 1);
                 }
             }
         }
@@ -230,7 +276,7 @@ public class HomeController implements Initializable {
 
     public WritableImage colorNoiseWhite(HashMap<Integer, Integer> hMap, HashMap<Integer, Integer> hMap2, int[] arr, int[] arr2, int percentNoiseReduction) {
 //        hMap = arrayToHashMap(arr);
-        hMap.putAll(hMap2);
+//        hMap.putAll(hMap2);
         Image im = imageViewEdited.getImage();
         PixelReader pr = im.getPixelReader();
 //        int per = percentNoiseReduction;
@@ -243,17 +289,29 @@ public class HomeController implements Initializable {
         for (int y = 0; y < im.getHeight(); y++) {
             for (int x = 0; x < im.getWidth(); x++) {
                 int pix = y * (int) im.getWidth() + x;
-                if (Find.findIntArr(arr, pix) > -1 || Find.findIntArr(arr2, pix) > -1) {//] > -1) {
+//                if (Find.findIntArr(arr, pix) > -1 || Find.findIntArr(arr2, pix) > -1) {//] > -1) {
+//                if (Find.findIntArr(arr, pix) == 17718){
+//                        System.out.println("Hello");
+//                    if (Find.findIntArr(arr, pix) > -1) {//] > -1) {
 //                    if ((hMap.containsKey(arr[pix]) || hMap.containsKey(arr2[pix])) && ((hMap.get(arr[pix]) < percentNoiseReduction) || (hMap.get(arr2[pix]) < percentNoiseReduction))) {
-                    if ((hMap.containsKey(Find.findIntArr(arr, pix)) && (hMap.get(Find.findIntArr(arr, pix)) < percentNoiseReduction)) || (hMap.containsKey(Find.findIntArr(arr2, pix)) && hMap.get(Find.findIntArr(arr2, pix)) < percentNoiseReduction)) {//[pix]) && (hMap.get(arr[pix]) < percentNoiseReduction))){ //|| (hMap.containsKey(arr2[pix]) && (hMap.get(arr2[pix]) < percentNoiseReduction))) {
-//                                    System.out.println("Pixel in noise reduction: x: " + x + ", y: " + y );
+                if (Find.findIntArr(arr, pix) > -1 && hMap.containsKey(Find.findIntArr(arr, pix)) && (hMap.get(Find.findIntArr(arr, pix)) < percentNoiseReduction))
+//                arr[pix] = -1;
+                    pixelWriter.setColor(x, y, Color.WHITE);
+//                hMap.put(Find.findIntArr(arr,pix), -1);
+                //|| (hMap2.containsKey(Find.findIntArr(arr2, pix)) && hMap2.get(Find.findIntArr(arr2, pix)) < percentNoiseReduction)) {//[pix]) && (hMap.get(arr[pix]) < percentNoiseReduction))){ //|| (hMap.containsKey(arr2[pix]) && (hMap.get(arr2[pix]) < percentNoiseReduction))) {
+//                                    System.out.println("Pixel in noise reduction: x: " + x + ", y: " + y );||
+//                        if (Find.findIntArr(arr2, pix) > -1) {
+                if (Find.findIntArr(arr2, pix) > -1 && hMap2.containsKey(Find.findIntArr(arr2, pix)) && hMap2.get(Find.findIntArr(arr2, pix)) < percentNoiseReduction) {
+
 //                                    System.out.println("hmap key: " + hMap.containsKey(arr[y * (int) im.getWidth() + x]));
 //                    System.out.println("in first if white noise reduction");
-                        pixelWriter.setColor(x, y, Color.WHITE);
-                    }
+//                    arr2[pix] = -1;
+                    pixelWriter.setColor(x, y, Color.WHITE);
+//                    hMap2.put(Find.findIntArr(arr2,pix), -1);
                 }
             }
         }
+
         return writableImage;
     }
 
@@ -263,7 +321,7 @@ public class HomeController implements Initializable {
             whiteCellMap = arrayToHashMap(whiteCellArray);
             System.out.println();
             redCellMap = arrayToHashMap(redCellArray);
-            int noisePercent = (int) noiseSlider.getValue() / 15;
+            int noisePercent = (int) noiseSlider.getValue();
 //            WritableImage wrImWhite = colorNoiseWhite(whiteCellMap, whiteCellArray, noisePercent);
             WritableImage wrIm = colorNoiseWhite(redCellMap, whiteCellMap, redCellArray, whiteCellArray, noisePercent);
             imageViewEdited.setImage(wrIm);
@@ -284,8 +342,13 @@ public class HomeController implements Initializable {
             WritableImage writableImage = new WritableImage(
                     (int) im.getWidth(), (int) im.getHeight());
             PixelWriter pixelWriter = writableImage.getPixelWriter();
+            double aspectRatio = im.getWidth() / im.getHeight();
+            double width = Math.min(imageViewEdited.getFitWidth(), imageViewEdited.getFitHeight() * aspectRatio);
+            double height = Math.min(imageViewEdited.getFitHeight(), imageViewEdited.getFitWidth() / aspectRatio);
             redCellArray = new int[(int) (im.getWidth() * (int) im.getHeight())];
+            System.out.println("redcellArray: " + redCellArray.length);
             whiteCellArray = new int[(int) (im.getWidth() * (int) im.getHeight())];
+            System.out.println("WhitecellArray: " + whiteCellArray.length);
             int whitePixel = -1;
 
             for (int y = 0; y < im.getHeight(); y++) {
@@ -303,7 +366,7 @@ public class HomeController implements Initializable {
                         whiteCellArray[(y * (int) im.getWidth()) + x] = whitePixel;
                     }
                     //setting purple 'white blood cells' nuclei
-                    else if (Math.min(r, b - 12) - g > 38) {// && b > g && b > r) {
+                    else if (Math.min(r, b - 12) - g > 35) {// && b > g && b > r) {
                         b = 160;
                         r = 140;
                         g = 75;
@@ -375,6 +438,11 @@ public class HomeController implements Initializable {
             imageViewEdited.setImage(null);
             imageViewEdited.setImage(freshImage);
             imageViewEdited.setEffect(colorAdjust);
+            edImagePane.getChildren().clear();
+            index = 0;
+            cellCount = 0;
+//            edImagePane2.getChildren().remove(obj);
+
 //            sepiaLabel.setText("Sepia : ");
 //            saturationLabel.setText("Saturation : ");
 //            brightnessLabel.setText("Brightness : ");
